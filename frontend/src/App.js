a7295c7d7e1dfd62a84c3b37326ed36990a61617
Nastya <anastasiazlobina264@gmail.com>
2025-10-22 17:19:20 +0300

    add HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Profile from "./components/Profile";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage"; // Добавьте этот импорт
 import './App.css';
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/" element={<h1 className="text-center mt-10 text-2xl">Главная страница</h1>} />
+        <Route path="/" element={<HomePage />} /> {/* Заменили на компонент HomePage */}
       </Routes>
     </BrowserRouter>
   );
